604b0904a4bb52925e8a0eb76b38a0b23d01944a
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-03-22 10:49:04 -0700

    Update layout server load function in +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,7 @@
 import type { LayoutServerLoad } from './$types'
-import type { PageServerLoad } from './$types'
 import { redirect } from '@sveltejs/kit'
 
-export const load: PageServerLoad = async (event) => {
+export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.auth()
 	if (!session?.user) throw redirect(303, '/auth')
 	return {}
